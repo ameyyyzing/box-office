edec821d9c8f78f78ebf4730c10e475f287377dc
ameya <pandavameya@gmail.com>
2021-06-11 20:47:47 +0530

    pages created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 
 function App() {
   return (
+    <div>
+      <Navs />
     <Switch>
       <Route exact path = "/">
-        This is home page
+        <Home />
       </Route>
       <Route exact path = "/starred">
-        This is starred page
+        <Starred />
       </Route>
-      <Route>This is 404 page</Route>
+      <Route>
+        <div>
+          Not Found!
+        </div>
+      </Route>
     </Switch>
+    </div>
   );
 }
 
