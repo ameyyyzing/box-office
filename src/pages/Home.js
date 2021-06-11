4f6793edf1cd94ecf6e04a0f2cc1ae6f20144fa3
ameya <pandavameya@gmail.com>
2021-06-12 02:42:09 +0530

    showed results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
+
 import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout';
+import {apiGet} from '../misc/config'
 
 const Home = () => {
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
 
     const onSearch = () => {
+        apiGet(`/search/shows?q=girls`).then(result =>{setResults(result)})
         // https://api.tvmaze.com/search/shows?q=girls
-        fetch(`http://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result =>{console.log(result)});
+       
      };    
 
     const onInputChange = ev => {
@@ -19,12 +23,24 @@ const Home = () => {
         }
     };
 
+    const renderResults = () =>{
+        if(results && results.length === 0){
+            return <div>No Results</div>
+        }
+        if(results && results.length > 0){
+            return <div>{results.map((item) =><div key={item.show.id}>{item.show.name}</div>)}</div>
+        }
+
+        return null;
+    };
+
     
 
     return (
         <MainPageLayout>
             <input type='text' onChange={onInputChange} onKeyDown={onKeyDown}value={input} />
             <button type="button" onClick={onSearch}>Search</button>
+            {renderResults()}
         </MainPageLayout>
     );
 };
