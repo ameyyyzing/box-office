2d142ff56d2961748dc0090a75efa580261d08b6
ameya <pandavameya@gmail.com>
2021-06-16 20:51:08 +0530

    styled navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
 
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       
     <Switch>
       <Route exact path = "/">
@@ -31,7 +40,7 @@ function App() {
 
       
     </Switch>
-    </div>
+    </ThemeProvider>
   );
 }
 
